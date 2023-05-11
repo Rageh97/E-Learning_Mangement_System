1ed5a56413e2e91ab299e1fb08f1097b70765e34
Muhammaad-Alaa <muhammedalaa1999@gmail.com>
2023-05-11 14:16:48 +0300

    edit structure of project and login

--- LMS_project/src/App.js
+++ LMS_project/src/App.js
@@ -1,15 +1,7 @@
 
 import React from "react";
 import {  RouterProvider  , createBrowserRouter} from "react-router-dom";
-// import Layout from "./components/Layout";
-// import AllCourses from "./components/CoursePags/AllCourses/index";
-// import List from "./components/CoursePags/ListOfCourses/index";
-// import Edit from "./components/CoursePags/EditCourse/index";
-// import CreateExam from "./components/Exame/Exam";
-// import Profile from "./components/edit/index";
-// import AddCourse from "./components/CoursePags/AddCourse/AddCourse";
-// import Course from "./components/CoursePags/course/index";
-// import Exame from "./components/examPage/index";
+
 import { allRoutes }  from './core/page'
 
 const router =  createBrowserRouter(allRoutes);
